33c5dd521192103c5684ee28216ff4a56e8a8fca
fevercatdream <100340974+fevercatdream@users.noreply.github.com>
2022-10-16 22:23:16 +0000

    working

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def hello_world(n):
-  for i in range(n):
-    print(f"Hello World {i+1}\n")
+jewlery_types = {
+  "earrings": 524.5, 
+  "necklace": 900, 
+  "ring": 550.75}
+
+gem_types = {
+  "ruby": 200, 
+  "sapphire": 230, 
+  "emerald": 400}
 
 
-hello_world(5)
+def buy_jewlery(jewelery_type, gems):
+  total = 0
+  total += jewlery_types[jewelery_type]
+  for i in range(len(gems)):
+    total += gem_types[gems[i]]
+  return total
+
+
+def list_to_string(my_list):
+  output = ''
+  for i in range(len(my_list)):
+    if i != len(my_list) - 1:
+      output += " & "
+  return output
+
+
+def string_to_list(my_string):
+  split_string = my_string.split(",")
+  for i in range(len(split_string)):
+    split_string[i] = split_string[i].strip()
+  return split_string
+
+
+jewlery = input("Select a jewlery type\n")
+gems = input("Select gems (comma delimited\n").strip().lower()
+
+value = buy_jewlery(jewlery, string_to_list(gems))
+
+print(value)
